760c7613a811cd42a8f3871edd0bf74bee205214
souvik-ghosal-007 <souvik13.12.2000@gmail.com>
2023-08-10 15:10:46 +0530

    scond

---
+++ api/index.js
@@ -0,0 +1,37 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+const PORT = 8000;
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+
+mongoose
+    .connect(
+        "mongodb+srv://root:i5xrtnnO33ntT7KX@cluster0.dbvyvzw.mongodb.net/",
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => {
+        console.log("DB Connection Established");
+    })
+    .catch((err) => {
+        console.log(
+            `There was an error in d Database connection: ${err.message}`
+        );
+    });
+
+app.get('/', (req, res) => {
+    res.status(200).json("Hello, Its Deployed");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running in port ${PORT}`);
+});
