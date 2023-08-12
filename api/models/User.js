4396f3ac3a7a116be18795d3dc5cd63113a512d9
souvik-ghosal-007 <souvik13.12.2000@gmail.com>
2023-08-12 15:13:41 +0530

    validator added

--- api/models/User.js
+++ api/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -43,6 +44,8 @@ const userSchema = new mongoose.Schema({
     },
   ],
 });
+
+userSchema.plugin(uniqueValidator);
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
